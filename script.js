d75fcd0d8afa8afe6f829cb75136c864115ba7a6
Thijssens <tibeau.thijssens@gmail.com>
2024-10-25 18:56:47 +0200

    register en start geven allebei de secrect weer.

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
-document.querySelector("#start_button").addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log("hallo");
-});
+const BASE_URL = "https://koene.cvoatweb.be/api/public/zeeslagje/";
+
+function getPassword() {
+  let password = document.querySelector("#password").value;
+  return password;
+}
+
+function getTeamName() {
+  let teamName = document.querySelector("#name").value;
+  return teamName;
+}
+
+async function startGame() {
+  let password = getPassword();
+
+  let response = await fetch(BASE_URL + "start", {
+    method: "POST",
+    body: JSON.stringify({ password }),
+  });
+  console.log(response);
+}
+
+async function registerTeam() {
+  let name = getTeamName();
+
+  let password = getPassword();
+
+  let response = await fetch(BASE_URL + "register/" + password, {
+    method: "POST",
+    body: JSON.stringify({ name }),
+  });
+
+  let secret = await response.json();
+  console.log(secret);
+}
+
+document
+  .querySelector("#start_button")
+  .addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    if (document.querySelector("#game_acces").value === "Maak spel aan") {
+      startGame();
+      registerTeam();
+    } else {
+      registerTeam();
+    }
+  });
