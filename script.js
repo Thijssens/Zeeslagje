1d5ab5e61fc94c1efaf7f05bbb2cf14374ba179d
Thijssens <tibeau.thijssens@gmail.com>
2024-11-07 17:30:56 +0100

    de schepen ophalen lukt en de secret is uit de global scope gehaald. Ik ga strakjes verder werken aan het plaatsen van de schepen, maar als je dit goed vind kan dit al gemerged worden

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://koene.cvoatweb.be/api/public/zeeslagje/";
 
-let secret = "";
-
+// let secret = ""; Global variabele is niet nodig: vanuit registerTeam(), functie playGame() aanroepen en zo de secret meegeven
+//---------------------------------------------MAAK SPEL---------------------------------------------------------------------------------------------------
 function getPassword() {
   let password = document.querySelector("#password").value;
   return password;
@@ -22,7 +22,6 @@ async function startGame() {
   console.log(response);
 }
 
-
 async function registerTeam() {
   let name = getTeamName();
 
@@ -34,49 +33,59 @@ async function registerTeam() {
   });
 
   let data = await response.json();
-  if (data.secret) {
-    secret = data.secret;
+
+  const secret = data.secret;
+
+  if (secret) {
+    playGame(secret);
   }
+
   console.log(secret);
 }
 
-document.querySelector("#start_button").addEventListener("click", async (event) => {
-    event.preventDefault();
+async function createGame() {
+  if (document.querySelector("#game_acces").value === "Maak spel aan") {
+    await startGame();
+    await registerTeam();
+  } else {
+    await registerTeam();
+  }
+}
+//--------------------------------------------SPEEL SPEL-----------------------------------------------------------------------
+async function getStatus(secret) {
+  const response = await fetch(BASE_URL + "status/" + secret);
+  const status = await response.json();
+  console.log("Game status:", status);
+}
 
-    if (document.querySelector("#game_acces").value === "Maak spel aan") {
-      startGame();
-      registerTeam();
-    } else {
-      registerTeam();
-    }
+async function getShips(secret) {
+  const response = await fetch(BASE_URL + "ships/" + secret);
+  const ships = await response.json();
+  console.log(ships);
+
+  let select = document.querySelector("#ship"); //select in HTML
+  document.querySelector("#ship_first_option").innerText =
+    "Selecteer een schip";
+
+  ships.forEach((ship) => {
+    // vult de select op
+    const option = document.createElement("option");
+    option.value = ship.name;
+    option.text = ship.quantity + " x " + ship.name;
+    select.add(option);
+  });
+}
+
+function playGame(secret) {
+  getStatus(secret);
+  getShips(secret);
+}
+
+document
+  .querySelector("#start_button")
+  .addEventListener("click", async (event) => {
+    event.preventDefault();
+    createGame();
   });
 
-
-
-
-async function getStatus() {  
-
-      const response = await fetch(BASE_URL + "status/" + secret);
-      if (response.ok) {
-          const status = await response.json();
-          console.log("Game status:", status); 
-      } else {
-          console.error("Failed to get game status.");
-          console.log(secret);
-      }
-      
-  }
-
-
-    document.querySelector("#status_button").addEventListener("click", getStatus);
-
-
- 
-
-  
-
-
-
-
-
-
+document.querySelector("#status_button").addEventListener("click", getStatus);
